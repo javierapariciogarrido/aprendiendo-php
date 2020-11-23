a5881c2087420a320dc7fa3d70a9a130dd80f0fd
Javier <javierapariciogarrido@gmail.com>
2020-11-23 21:38:51 +0100

    Comienzo proyectos php

---
+++ 02-imprimir/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE  HTML>
+<html lang="es">
+    <head>
+        <title>Imprimir por pantalla  - Master en PHP </title>
+        <meta charset="utf-8">
+    </head>
+    
+    <body>
+        <h1>Máster en PHP  - Victorroblesweb</h1>
+        
+        <?='Bienvenido al curso mas grande de PHP';?>
+        
+        <?php
+            // Titular de la sección
+            
+            /*Esta
+              es una lista de videojuegos 
+             modernos
+              */
+        
+            echo '<h3>Listado de Viodeojuegos:</h3>'.
+                   '<ul>'.
+                     '<li>GTA</li> '.
+                     '<li>Fifa</li>'.   
+                     '<li>Mario Bros</li>'.
+                   '</ul>'; 
+            
+            
+            
+            // Parrafo explicativo
+            echo '<p>Esta es toda'.' - '.'lista de juegos </p>';
+        ?>
+    </body>
+    
+</html>
+
+
+
+
---
+++ 04-tipos/index.php
@@ -0,0 +1,36 @@
+<?php
+
+/*Tipos de datos : 
+    Numero entero (int/integer):ej :99
+    Coma flotante/decimales (float /double): ej:3.45
+    Cadena de caracteres (string) : ej:"Hola yo soy un string"
+    Boleanos ( boolean), ej:true o false 
+    Nulo ej NULL
+    Array(Colección de datos)
+    Objetos
+ */
+
+$numero= 100;
+
+$decimal = 27.9;
+
+$texto="Soy un texto ' $numero";
+$verdadero=false;
+echo $verdadero;
+echo "<br>";
+
+echo $texto;
+
+$nula=null;
+
+// PARA SABER EL TIPO DE DATO QUE UNA VARIABLE CONTIENE USAMOS GETTYPE(VARIABLE) 
+//echo gettype($nula);
+
+//debuggear
+
+$mi_nombre[1]="Victor Robles";
+$mi_nombre[2]="Victor Robles";
+//var_dump($mi_nombre);
+
+
+
