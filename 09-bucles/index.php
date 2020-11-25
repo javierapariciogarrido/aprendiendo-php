4a39ffdc3c1e7ebfaab9ccbfef3845058bad4443
Javier <javierapariciogarrido@gmail.com>
2020-11-25 14:33:46 +0100

    Actualizacion de los ejercicios aprendiendo php con bucles por ultimo

--- 09-bucles/index.php
+++ 09-bucles/index.php
@@ -4,8 +4,48 @@
  BUCLE WHILE:ESTRUCTURA DE CONTROL QUE ITERA O REPITE LA EJECUCIÓN DE UNA SERIE DE INSTRUCCIONES
  * QUE CONTIENE DENTRO Y SE REPETIRAN ESAS INSTRUCCIONES MIENTRAS QUE LA CONDICIÓN PRINCIPAL DEL 
  * BUCLE SE CUMPLA
- */
+    while(condicion){
+        instrucciones
+       instrucciones
+       
+    }             
+ *  */
 
-
+$numero = 1;
+while ($numero<=100){
+    echo $numero;
+    if($numero!=100){
+        echo ", ";
+    }
+    $numero++;
+}
     
+echo "<hr>";
 
+//Ejemplo
+
+
+if(isset($_GET['numero'])){
+    $numero=$_GET['numero'];
+    echo "<h2>Tabla de multiplicar del numero $numero </h2>";
+    $iteracion=0;
+    while ($iteracion<=10){
+        echo "<p>$numero x $iteracion = ".($numero*$iteracion).'</p>';
+        $iteracion++;
+    }
+    
+}else{
+    echo "NO HAS INTRODUCIDO PARÁMETRO GET";
+}
+
+
+// DO WHILE : EJECUTA BLOQUE DE INSTRUCCIONES AL MENOS UNA VEZ Y LUEGO EVALUA LA CONDICIÓN,
+// SI LA CONDICIÓN SE CUMPLE ENTRA EN EL BUCLE POR SEGUNDA VEZ,SINO SALE PERO MINIMO SE EJECUTAN
+//LAS INSTRUCCIONES UNA VEZ 
+echo "<hr>";
+$edad = 18;
+$contador=1;
+do{
+    echo "<h2>tienes acceso al local privado $contador</h2>";
+    $contador++;
+}while($edad>=18 && $contador <=10);
---
+++ 10-bucle-for/index.php
@@ -0,0 +1,17 @@
+<?php
+
+/* 
+ BUCLE FOR: EL BUCLE EN SU ESTRUCTURA DEFINE LA INICIALIZACIÓN DEL CONTADOR,LA CONDICIÓN DE ENTRADA
+ * AL BUCLE Y EL INCREMENTO O DECREMENTO DEL CONTADOR Y YA LUEGO DENTRO TIENE EL BLOQUE DE 
+ * INSTRUCCIONES  
+ */
+$suma=0;
+for($i=1;$i<=100;$i++){
+   $suma +=$i; // Esto equivale a poner :$suma = $suma + $i 
+}
+
+echo "El resultado de la suma de los numeros del 1 al 100 es : $suma";
+
+
+
+
